349f88df4c9c273cbf7a9be9ea5ae650af74fb3a
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2025-04-07 20:40:32 +0530

    Create Slip9_Q2.c

---
+++ Slip9_Q2.c
@@ -0,0 +1,103 @@
+/*
+Implement a Binary search tree (BST) library (btree.h) with operations – create, insert, post order. Write 
+a menu driven program that performs the above operations 
+*/
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *left,*right;
+};
+
+struct node*create(struct node*root){
+    int limit,i;
+    struct node*newnode,*p,*q;
+    printf("\nENTER A LIMIT:");
+    scanf("%d",&limit);
+    for(i=0;i<limit;i++)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("ENTER VALUE:");
+        scanf("%d",&newnode->data);
+        newnode->left=NULL;
+        newnode->right=NULL;
+        if(root==NULL)
+        {
+            root=newnode;
+        }
+        else
+        {
+            p=root;
+            while(p!=NULL)
+            {
+                q=p;
+                if(newnode->data<p->data)
+                p=p->left;
+                else
+                p=p->right;
+            }
+            if(newnode->data<q->data)
+            q->left=newnode;
+            else
+            q->right=newnode;
+        }
+    }
+    return root;
+}
+
+struct node* insert(struct node*root,int num){
+    struct node*newnode,*p,*q;
+    newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->data=num;
+    newnode->left=NULL;
+    newnode->right=NULL;
+    if(root==NULL){
+        return newnode;
+    }
+    p=root;
+    while(p!=NULL){
+        q=p;
+        if(newnode->data<p->data)
+        p=p->left;
+        else
+        p=p->right;
+    }
+    if(newnode->data<q->data)
+    q->left=newnode;
+    else
+    q->right=newnode;
+    printf("\nINSERT SUCC!");
+    return root;
+}
+
+void postorder(struct node*root){
+    if(root!=NULL){
+        postorder(root->left);
+        postorder(root->right);
+        printf("\t%d",root->data);
+    }
+}
+int main(){
+    struct node*root=NULL;
+    int choice,num;
+    while(1){
+        printf("\n1:CREATE\n2:INSERT\n3:POSTORDER\n4:EXIT");
+        printf("\nENTER A CHOICE:");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:root=create(root);
+                    break;
+            case 2:printf("\nENTER A NUMBER TO INSERT:");
+                    scanf("%d",&num);
+                   root=insert(root,num);
+                   break;
+            case 3:printf("\nELEMENTS OF TREE IN POSTORDER:");
+                    postorder(root); 
+                    break;
+            case 4:printf("\nEXIT SUCC !");
+                    return 0;
+                    break;
+            default:printf("\nINVALID CHOICE");
+        }
+    }
+}
